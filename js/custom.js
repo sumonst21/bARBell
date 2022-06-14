9235b33ff09445c887fa283bc9eada1b6be46848
BrandonBell2025 <107322807+BrandonBell2025@users.noreply.github.com>
2022-06-14 00:47:26 -0400

    updated nav bar

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	console.log("loaded ;)...");
+
+	$("#calculator").click(function() {
+    	$("body").css("background-color", "#000");
+    	$("body").css("opacity", "70%");
+
+  	});
+});
