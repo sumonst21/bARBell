ed3f603361dd983bb2cbf55b64d92316ba1862a4
Robert Baxter Jr <rsb2194@columbia.edu>
2022-06-14 14:43:58 -0400

    Draftkings Odds Output (Incomplete)
    Incomplete and csv file created

--- bARBell.py
+++ bARBell.py
@@ -1,10 +1,36 @@
+from tokenize import Name
 import requests
 import re
 import pandas as pd
 from bs4 import BeautifulSoup
+import time
 
-web = 'https://www.pinnacle.com/en/baseball/matchups'
+
+web = 'https://sportsbook.draftkings.com/leagues/baseball/88670847'
 page = requests.get(web)
-soup = BeautifulSoup(page.text, 'html.parser')
-odds =[ soup.find_all('span', attrs = {'class' : 'style_price__15SlF'})]
-print(odds)
+from csv import writer
+time.sleep(1)
+soup = BeautifulSoup(page.content, 'html.parser')
+odds = soup.find('tbody', class_='sportsbook-table__body')
+
+with open('odds.csv', 'w', newline = '', encoding = 'utf8') as f :
+    thewriter = writer(f)
+    header = ['DraftKings', 'Team', 'Moneyline', 'Total', 'Run Line']
+    sportsbook = 'DRAFTKINGS'
+    thewriter.writerow(header)
+
+    for odd in odds :
+        name = odd.find('div', class_='event-cell__name-text').text.replace('\n', '')
+        moneyline = odd.find('span', class_='sportsbook-odds american no-margin default-color').text.replace('\n', '')
+        total1 = soup.find('div', class_='sportsbook-outcome-cell')
+        for total2 in total1 :
+            total = total2.find('span', class_='sportsbook-odds american default-color')
+        runline = odd.find('span', class_='sportsbook-odds american default-color').text.replace('\n', '')
+        info = [name, moneyline, total, runline]
+        thewriter.writerow(info)
+        #print(f'Team : {name.text}')
+        #print(f'Moneyline : {moneyline.text}')
+        #print(f'Total : {total.text}')
+        #print(f'Run Line : {runline.text}')
+        #print('-------------------------------------------------------------------------')
+
